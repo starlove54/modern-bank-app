9c82c02daa3b1948eff1a4512c3c75a07de9afbe
Anubhav Negi <anubhavnegi54@gmail.com>
2024-02-16 13:44:45 +0530

    adding all files

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Billing from './Billing'
+import Business from './Business'
+import Hero from './Hero'
+import Button from './Button'
+import CardDeal from './CardDeal'
+import Clients from './Clients'
+import CTA from './CTA'
+import FeedbackCard from './FeedbackCard'
+import Footer from './Footer'
+import GetStarted from './GetStarted'
+import Navbar from './Navbar'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+
+export {
+  Billing,
+  Business,
+  Hero,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Navbar,
+  Stats,
+  Testimonials,
+}
